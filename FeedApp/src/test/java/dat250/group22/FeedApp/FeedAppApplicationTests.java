a2cabfa029565a849664de0fdcfe69a10ecf4af6
KaroGil <karolinkagil@gmail.com>
2024-10-17 10:41:35 +0200

    Initial commit

---
+++ FeedApp/src/test/java/dat250/group22/FeedApp/FeedAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dat250.group22.FeedApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
