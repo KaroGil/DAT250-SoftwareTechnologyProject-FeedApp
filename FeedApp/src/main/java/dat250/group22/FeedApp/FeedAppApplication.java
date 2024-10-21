453ae10b1366655120cd4507f1eaf42bcfd682f4
Mampenda <amalie.mampenda@gmail.com>
2024-10-21 09:19:46 +0200

    Started on Dockerfile

--- FeedApp/src/main/java/dat250/group22/FeedApp/FeedAppApplication.java
+++ FeedApp/src/main/java/dat250/group22/FeedApp/FeedAppApplication.java
@@ -2,12 +2,20 @@ package dat250.group22.FeedApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class FeedAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FeedAppApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Docker World!";
 	}
 
 }
