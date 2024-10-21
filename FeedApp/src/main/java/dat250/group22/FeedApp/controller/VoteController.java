1d411e232c905e24138e3e84378fda29c04d6aa0
Karolina Gil <60822728+KaroGil@users.noreply.github.com>
2024-10-21 09:18:28 +0200

    Setup vote and voteOption (#21)

--- FeedApp/src/main/java/dat250/group22/FeedApp/controller/VoteController.java
+++ FeedApp/src/main/java/dat250/group22/FeedApp/controller/VoteController.java
@@ -1,5 +1,50 @@
 package dat250.group22.FeedApp.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dat250.group22.FeedApp.manager.DomainManager;
+import dat250.group22.FeedApp.model.Vote;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import java.util.Collection;
+import java.util.UUID;
+
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/vote")
 public class VoteController {
-    
+    private final DomainManager manager;
+
+    public VoteController(DomainManager manager) {
+        this.manager = manager;
+    }
+
+    @GetMapping
+    public Collection<Vote> getVotes() {
+        return manager.getAllVotes();
+    }
+
+    @PostMapping
+    public void createVote(@RequestBody Vote vote) {
+        Vote newVote = new Vote(vote.getVotedBy(), vote.getVoteOptionId(), vote.getPollId());
+        manager.addVote(newVote);
+    }
+
+    @DeleteMapping("/{voteId}")
+    public void deleteVote(@PathVariable UUID voteId) {
+        manager.removeVote(voteId);
+    }
+
+    @PutMapping("/{voteId}")
+    public void updateVote(@PathVariable UUID voteId, @RequestBody Vote newVote) {
+        manager.updateVote(voteId, newVote);
+    }
 }
--- FeedApp/src/main/java/dat250/group22/FeedApp/model/VoteOption.java
+++ FeedApp/src/main/java/dat250/group22/FeedApp/model/VoteOption.java
@@ -1,5 +1,35 @@
 package dat250.group22.FeedApp.model;
+import java.util.UUID;
 
-public class VoteOption {
-    
+public class VoteOption  implements java.io.Serializable {
+    private String caption;
+    private int presentationOrder;
+    private UUID id;
+
+    public VoteOption(String caption, int presentationOrder) {
+        this.id = UUID.randomUUID();
+        this.caption = caption;
+        this.presentationOrder = presentationOrder;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getCaption() {
+        return caption;
+    }
+
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
+
+    public int getPresentationOrder() {
+        return presentationOrder;
+    }
+
+    public void setPresentationOrder(int presentationOrder) {
+        this.presentationOrder = presentationOrder;
+    }
+
 }
