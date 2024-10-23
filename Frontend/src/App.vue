40cdce9fabe890de66d428c0bbc3d09dc2448db7
KaroGil <karolinkagil@gmail.com>
2024-10-23 13:33:11 +0200

    add basic homepage

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,53 +1,26 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
 </script>
 
 <template>
   <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
+    <h1>Feed App</h1>
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <p>Best group ever == group22 == {names: [Kaja, Karolina, Mina, Mampenda]}</p>
     </div>
   </header>
-
   <main>
-    <TheWelcome />
+    <button @click="change">
+      Login
+    </button>
+    <button @click="count++">
+      Vote
+    </button>
+    <button @click="count++">
+      See polls
+    </button>
+    <button @click="count++">
+      See users
+    </button>
   </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
